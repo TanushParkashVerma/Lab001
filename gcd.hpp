f75fcc87b9b4c97fc60f200fd160483285fd2e61
TanushParkashVerma <tanush.parkash16@gmail.com>
2019-09-13 19:59:05 -0700

    Modified Hello World To Hello, CLion

--- gcd.hpp
+++ gcd.hpp
@@ -1,8 +1,27 @@
 //
 // Created by tanus on 2019-09-13.
 //
+//Greatest Common Denomonitor
+//PRE: a is a positive integer
+//PRE: b is a positive integer
+//POST: a and b are unchanged
+//RETURN: the greatest common denominator of
+// a and b
 
 #ifndef HELLOCLION_GCD_HPP
 #define HELLOCLION_GCD_HPP
+
+int gcd(const int a, const int b){
+    int GCD=0;
+    for (int i=1; i<=a && i<=b; i++){
+        if(a%i==0 && b%i==0){
+            GCD=i;
+        }
+    }
+    return GCD;
+
+}
+
+
 
 #endif //HELLOCLION_GCD_HPP
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "gcd.hpp"
 
 int main() {
-    std::cout << "Hello, CLion" << std::endl;
+
+    int a=2, b=4;
+
+    std::cout << "gcd of a and b is " << gcd(a,b) << std::endl;
     return 0;
 }
